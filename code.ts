f6381a4574b9c3c182d13fbc151bdbdb9614d384
Alexander Lapikov <racman996@gmail.com>
2025-02-26 15:55:17 +0300

    added two new functions

--- code.ts
+++ code.ts
@@ -11,3 +11,13 @@ export function getRepeatingChars(arrOfChars: string[] | number[]): {[key: strin
         return b - a;
     }));
 }
+
+export function sumDigitsOfNumber(num: number): number {
+    return num.toString().split('').map(Number).reduce((prev, curr) => prev + curr);
+}
+
+export function getUrlDomain(url: string) {
+    const cutUrl = url.replace(/https?:\/\/|www\./g, '');
+    const idx = cutUrl.indexOf('/');
+    return idx !== -1 ? cutUrl.substring(0, idx) : cutUrl;
+}
